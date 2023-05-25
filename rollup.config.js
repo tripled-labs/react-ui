25ccac590b0ab8762e8a21f0208d4e28d34ebae2
Aen Jojo <josuafernando999@gmail.com>
2023-05-26 01:02:02 +0700

    chore(bundle): configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import { defineConfig } from 'rollup';
+import packageJson from './package.json' assert { type: 'json' };
+
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import peerDepsExt from 'rollup-plugin-peer-deps-external';
+
+export default defineConfig([
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: packageJson.main,
+				format: 'cjs',
+				sourcemap: true,
+			},
+			{
+				file: packageJson.module,
+				format: 'esm',
+				sourcemap: true,
+			},
+		],
+		plugins: [
+			peerDepsExt(),
+			resolve(),
+			commonjs(),
+			typescript({
+				tsconfig: './tsconfig.json',
+				exclude: [
+					'stories/**/*.stories.tsx',
+					'tests/**/*.test.tsx',
+				],
+			}),
+			terser(),
+		],
+		external: [
+			'react-dom',
+		],
+	},
+	{
+		input: 'dist/types/index.d.ts',
+		output: {
+			file: packageJson.types,
+			format: 'esm',
+		},
+		plugins: [
+			dts(),
+		],
+		external: [
+			/\.css$/,
+		],
+	},
+]);
